21ab3ceec711774d2e0ca35506e1f712fbcf4488
Anke <Anke@Anke-PC>
2011-06-29 07:53:48 +0200

    Do I need a command "public void getPoseReceiver()" in the class Pointer? I don't know how this one looks like.

--- src/de/tum/in/far/threedui/bartender/Pointer.java
+++ src/de/tum/in/far/threedui/bartender/Pointer.java
@@ -1,17 +1,41 @@
 package de.tum.in.far.threedui.bartender;
 
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.TransformGroup;
+
+import de.tum.in.far.threedui.bartender.UbitrackFacade;
+import de.tum.in.far.threedui.ex2.solution.PoseReceiver;
+
 public class Pointer extends TransformableObject {
 	
 	PoseReceiver pointerPoseReceiver;
+	private UbitrackFacade ubitrackFacade;
 	
 	public Pointer() {
 		// TODO Anke: create a posereceiver and store it as a field
+		
+		
+		pointerPoseReceiver = new PoseReceiver();
+		if (!ubitrackFacade.setPoseCallback("posesink", pointerPoseReceiver)) {
+		return;
+		}
+		ubitrackFacade.startDataflow();
+		
 	}
 	
 	public void setModel(ModelObject model) {
 		// TODO Anke: store this model and use it
+		BranchGroup bg = new BranchGroup();
+		TransformGroup offset = new TransformGroup();
+		Transform3D t3d = new Transform3D();
+		
+		offset.setTransform(t3d);
+		bg.addChild(offset);
+		offset.addChild(model);
+		
 	}
-
+	
 	/**
 	 * @param args
 	 */
@@ -22,9 +46,11 @@ public class Pointer extends TransformableObject {
 		
 		UbitrackManager um = new UbitrackManager();
 		Pointer p = new Pointer();
-		p.setModel(um.loadModel("Sheep.wrl"));
-		um.linkReceiverToMarker(p.getPoseReceiver(), "posesink");
+		p.setModel(um.loadModel("Sheep.wrl"));//p.getPoseReceiver()
+		um.linkReceiverToMarker("posesink",p.getPoseReceiver());
 		um.startTracking();
 	}
+
+
 
 }
