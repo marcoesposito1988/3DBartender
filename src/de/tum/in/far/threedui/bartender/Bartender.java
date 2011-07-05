9517f449b4d2023cc465b34288e8dfb0c877f13d
Marco <marcoesposito1988@gmail.com>
2011-07-05 20:59:37 +0200

    Glass and menu (of sheep) visible.

--- src/de/tum/in/far/threedui/bartender/Bartender.java
+++ src/de/tum/in/far/threedui/bartender/Bartender.java
@@ -6,6 +6,7 @@ public class Bartender {
 	
 	Menu menu = new Menu();
 	Pointer pointer = new Pointer();
+	Glass glass = new Glass();
 	
 	public Bartender() {
 		ubitrackManager = new UbitrackManager();
@@ -18,15 +19,19 @@ public class Bartender {
 		
 		ubitrackManager.prepareTracking();
 
-		PoseReceiver pr = ubitrackManager.getReceiverForMarker("posesink");
-		PoseReceiver pr2 = ubitrackManager.getReceiverForMarker("posesink3");
+		PoseReceiver pointerReceiver = ubitrackManager.getReceiverForMarker("posesink");
+		PoseReceiver menuReceiver = ubitrackManager.getReceiverForMarker("posesink3");
+		PoseReceiver glassReceiver = ubitrackManager.getReceiverForMarker("posesink4");
 
 		ubitrackManager.startTracking();
+		
 		ubitrackManager.addObjectToViewer(pointer);
 		ubitrackManager.addObjectToViewer(menu);
+		ubitrackManager.addObjectToViewer(glass);
 		
-		pointer.setPoseReceiver(pr);
-		menu.setPoseReceiver(pr2);
+		pointer.setPoseReceiver(pointerReceiver);
+		menu.setPoseReceiver(menuReceiver);
+		glass.setPoseReceiver(glassReceiver);
 	}
 
 	/**
